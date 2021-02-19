441fa0723bfa79024b3330fe611d4279848248f8
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2021-02-19 09:58:30 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LesBouclesExo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Afficher 10 fois le message suivant dans la console :
+            //«Bonjour, je ne suis qu'un simple message.»
+
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine($"Bonjour, je ne suis qu'un simple message {counter}");
+                counter++;
+            }
+        }
+    }
+}
